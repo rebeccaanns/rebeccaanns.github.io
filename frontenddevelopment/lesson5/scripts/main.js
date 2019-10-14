4fbbdc852b4ffb68d654946bbaa53e8412114727
rebeccaanns <stu18005@byui.edu>
2019-10-14 10:12:45 -0600

    lesson 5

---
+++ frontenddevelopment/lesson5/scripts/main.js
@@ -0,0 +1,42 @@
+var currentDate = new Date();
+var dateString;
+
+var weekDayNum = currentDate.getDay();
+var monthNum = currentDate.getMonth();
+
+var daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+
+var weekDay = daysOfWeek[weekDayNum];
+
+var months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+var monthName = months[monthNum];
+
+dateString = weekDay + ", " + currentDate.getDate() + " " + monthName + " " + currentDate.getFullYear();
+
+document.getElementById("currentDate").innerHTML = dateString;
+
+function toggleMenu(){
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
