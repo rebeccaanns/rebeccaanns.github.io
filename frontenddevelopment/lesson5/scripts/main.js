3eb2e33fce51de4e25c446db40edd1006e2e85ba
rebeccaanns <stu18005@byui.edu>
2019-10-18 10:27:38 -0600

    port

--- frontenddevelopment/lesson5/scripts/main.js
+++ frontenddevelopment/lesson5/scripts/main.js
@@ -1,9 +1,11 @@
 var currentDate = new Date();
 var dateString;
 
+//current date 
 var weekDayNum = currentDate.getDay();
 var monthNum = currentDate.getMonth();
 
+//day of week not number
 var daysOfWeek = [
     'Sunday',
     'Monday',
@@ -39,4 +41,12 @@ document.getElementById("currentDate").innerHTML = dateString;
 
 function toggleMenu(){
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+//5 day forecast
+let day1, day2, day3, day4, day5;
+
+if(weekDayNum < 6){
+    day1 = daysOfWeek[weekDayNum + 1];
+    document.getElementById("day1").innerHTML = day1;
 }
