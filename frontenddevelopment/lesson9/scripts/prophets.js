f2d542b7cabd74721cd747e88ce62f075f71f13f
rebeccaanns <stu18005@byui.edu>
2019-11-13 21:01:48 -0700

    prophets javascript

--- frontenddevelopment/lesson9/scripts/prophets.js
+++ frontenddevelopment/lesson9/scripts/prophets.js
@@ -4,9 +4,62 @@ fetch(requestURL)
     .then(response => {
         response.json()
             .then(
-                person => {
-                    console.table(person.prophets);
-                    let prophet = (person);
+                response => {
+                    const prophets = response.prophets;
+                    console.table(prophets);
+
+                    prophets.forEach(
+                        (prophet) => {
+                            let card = document.createElement('article');
+                            let h2 = document.createElement('h2');
+
+                            h2.textContent = prophet.name + ' ' + prophet.lastname;
+
+                            card.appendChild(h2);
+
+                            //add date of birth
+                            let birthdateDiv = document.createElement('div');
+
+                            let birthdateLabel = document.createElement('label');
+                            birthdateLabel.textContent = "Date of Birth: ";
+
+                            let birthdateSpan = document.createElement('span');
+                            birthdateSpan.textContent = prophet.birthdate;
+
+                            birthdateDiv.appendChild(birthdateLabel);
+                            birthdateDiv.appendChild(birthdateSpan);
+
+                            card.appendChild(birthdateDiv);
+
+                            //add place of birth
+                            let birthplaceDiv = document.createElement('div');
+
+                            let birthplaceLabel = document.createElement('label');
+                            birthplaceLabel.textContent = "Place of Birth: ";
+
+                            let birthplaceSpan = document.createElement('span');
+                            birthplaceSpan.textContent = prophet.birthplace;
+
+                            birthplaceDiv.appendChild(birthplaceLabel);
+                            birthplaceDiv.appendChild(birthplaceSpan);
+
+                            card.appendChild(birthplaceDiv);
+
+                            //add image
+                            let imageDiv = document.createElement('div');
+
+                            let image = document.createElement('img');
+                            
+                            image.setAttribute('src', prophet.imageurl);
+                            image.setAttribute('alt' , prophet.name + " " + prophet.lastname + " - " + prophet.order);
+
+                            imageDiv.appendChild(image);
+
+                            card.appendChild(imageDiv);
+
+                            document.querySelector('section.cards').appendChild(card);
+                        }
+                    );
                 });
     });
 
