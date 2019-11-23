7d98afa51fde9b8a740ff75e92209fdef8f036c0
rebeccaanns <stu18005@byui.edu>
2019-11-23 14:52:21 -0700

    commit

--- frontenddevelopment/lesson10/scripts/main.js
+++ frontenddevelopment/lesson10/scripts/main.js
@@ -58,8 +58,69 @@ for (let i = 1; i < 6; i++) {
     element.innerHTML = daysOfWeek[currentDay];
 }
 
+//API for 5 day forecast
+const apiForecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=e22134d7e2f991aa104d13141ea859d1&units=imperial";
+
+fetch(apiForecastURL)
+    .then((response) => response.json())
+    .then((forecasts) => {
+        //DEBUG
+        console.log(forecasts);
+
+        //loop through results
+        forecasts.list.forEach(
+            (forecast) => {
+                let nextDate = new Date();
+                nextDate.setDate(nextDate.getDate() + 1);
+                let dateString = getDateString(nextDate);
+                let hourString = "18:00:00";
+                let counter = 1;
+
+                forecasts.list.forEach(
+                    (forecast) => {
+                        if (forecast.dt_txt.includes(dateString) && forecast.dt_txt.includes(hourString)) {
+                            // console.log(forecast.main.temp);
+
+                            const temp = document.getElementById(`temp${counter}`);
+                            temp.innerHTML = Math.round(forecast.main.temp) + "&deg;";
+
+                            // const icon = document.getElementById(`icon${counter}`);
+                            let apiIcon = forecast.weather[0].icon;
+                            let currentImg = "https://openweathermap.org/img/w/" + apiIcon + ".png";
+                            const description  = forecast.weather[0].description;
+
+                            document.getElementById(`icon${counter}`).setAttribute('src', currentImg);
+                            document.getElementById(`icon${counter}`).setAttribute('alt', description);
+                            // icon.innerHTML = currentImg;
+
+                            counter++;
+                            nextDate.setDate(nextDate.getDate() + 1);
+                            dateString = getDateString(nextDate);
+                        }
+                    }
+                )
+            }
+        );
+    });
+
+function getDateString(date) {
+    let dateString = date.getFullYear() + '-' +
+        (date.getMonth() + 1) + '-' +
+        date.getDate();
+        
+    return dateString;
+}
+
+// function icon() {
+//     let currentIcon = currentWeather.weather[0].icon;
+//         let currentImg = "https://openweathermap.org/img/w/" + currentIcon + ".png";
+//         document.querySelector("#imagesrc").textContent = currentImg;
+//         document.querySelector("#weather-icon").setAttribute('src', currentImg);
+//         document.querySelector("#weather-icon").setAttribute("alt", currentWeather.weather[0].main);
+// }
+
 //pancake
-if (weekDayNum === 5){
+if (weekDayNum === 5) {
     document.getElementById("pancake").removeAttribute("class", "hidden");
 }
 
@@ -67,5 +128,3 @@ if (weekDayNum === 5){
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
-
-//home page
