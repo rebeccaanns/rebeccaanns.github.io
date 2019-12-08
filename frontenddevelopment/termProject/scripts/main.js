6ff915848f5d15ace8d2405115315cd1581ff48a
rebeccaanns <stu18005@byui.edu>
2019-12-07 23:35:49 -0700

    home

--- frontenddevelopment/termProject/scripts/main.js
+++ frontenddevelopment/termProject/scripts/main.js
@@ -1,3 +1,33 @@
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+
+//SLIDES
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    let slides = document.getElementsByClassName("slides");
+
+    if (n > slides.length){
+        slideIndex = 1;
+    }
+
+    if (n < 1){
+        slideIndex = slide.length;
+    }
+
+    for(var i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex-1].style.display = "block";
+}
